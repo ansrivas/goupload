077752d9d6922cad8aaf5277bcf9e06dbedcc430
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-05-31 23:53:28 +0200

    initial commit

---
+++ internal/conf_reader.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+var (
+	config *viper.Viper
+	//ErrConfigUninitalized is thrown whenever a configuration is not initialized.
+	ErrConfigUninitalized = fmt.Errorf("Config uninitialized. Please call InitConfig first")
+)
+
+// InitConfig returns the config object for this project
+func InitConfig(configPath string) error {
+	viper.SetConfigFile(configPath)
+	err := viper.ReadInConfig()
+	if err != nil {
+		return errors.New("Config file not found: " + configPath)
+	}
+	config = viper.GetViper()
+	return nil
+}
+
+//GetConfig returns the viper config object
+func GetConfig() (*viper.Viper, error) {
+	if config != nil {
+		return config, nil
+	}
+	return nil, ErrConfigUninitalized
+}
---
+++ internal/conf_reader_test.go
@@ -0,0 +1,25 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetConfig(t *testing.T) {
+	assert := assert.New(t)
+
+	conf, err := GetConfig()
+	assert.NotNil(err, "Error should be not nil")
+	assert.Equal(err.Error(), ErrConfigUninitalized.Error())
+
+	err = InitConfig("config.yaml")
+	assert.Nil(err, "File present, error should be nil")
+	conf, err = GetConfig()
+	expected := "world"
+	assert.Equal(expected, conf.GetString("field.hello"), "expected to read world from the config")
+
+	err = InitConfig(".")
+	assert.NotNil(err, "File NOT present, error should be notnil")
+
+}
